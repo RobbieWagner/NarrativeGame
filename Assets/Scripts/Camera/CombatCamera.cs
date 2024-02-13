630a320c9a83880c7865e2aacf6aa97cf93f7ee2
Robbie Wagner <robertmwagneriii@gmail.com>
2024-02-13 17:15:10 -0600

    #19 complete

--- Assets/Scripts/Camera/CombatCamera.cs
+++ Assets/Scripts/Camera/CombatCamera.cs
@@ -5,9 +5,6 @@ using DG.Tweening;
 
 public class CombatCamera : GameCamera
 {
-
-    public Vector3 defaultPosition = Vector3.zero;
-
     public static CombatCamera Instance {get; private set;}
 
     protected override void Awake()
@@ -22,26 +19,5 @@ public class CombatCamera : GameCamera
         }
         
         base.Awake(); 
-    }
-
-    public IEnumerator MoveCamera(Vector3 position, float time = 1f)
-    {
-        yield return transform.DOMove(position, time).WaitForCompletion();
-    }
-    public IEnumerator MoveCameraSpeed(Vector3 position, int speed = 1)
-    {
-        if(speed < 1) StopCoroutine(MoveCamera(position, speed)); 
-        yield return transform.DOMove(position, Vector3.Distance(transform.position, position)/speed).WaitForCompletion();
-    }
-
-    public IEnumerator ResetCameraPosition(float time = 1f)
-    {
-        yield return transform.DOMove(defaultPosition, time).WaitForCompletion();
-    }
-
-    public IEnumerator ResetCameraPositionSpeed(int speed = 1)
-    {
-        if(speed < 1) StopCoroutine(ResetCameraPosition(speed)); 
-        yield return transform.DOMove(defaultPosition, Vector3.Distance(transform.position, defaultPosition)/speed).WaitForCompletion();
     }
 }
--- Assets/Scripts/Camera/GameCamera.cs
+++ Assets/Scripts/Camera/GameCamera.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class GameCamera : MonoBehaviour
 {
     public Camera cam;
     public bool switchToOnEnable = false;
+    public Vector3 defaultPosition = Vector3.zero;
 
     public void ActivateCamera()
     {
@@ -26,5 +28,27 @@ public class GameCamera : MonoBehaviour
     {
         if(switchToOnEnable)
             CameraManager.Instance?.TrySwitchGameCamera(this);
+    }
+
+    public virtual IEnumerator MoveCamera(Vector3 position, float time = 1f)
+    {
+        yield return transform.DOMove(position, time).WaitForCompletion();
+    }
+    
+    public virtual IEnumerator MoveCameraSpeed(Vector3 position, int speed = 1)
+    {
+        if(speed < 1) StopCoroutine(MoveCamera(position, speed)); 
+        yield return transform.DOMove(position, Vector3.Distance(transform.position, position)/speed).WaitForCompletion();
+    }
+
+    public virtual IEnumerator ResetCameraPosition(float time = 1f)
+    {
+        yield return transform.DOMove(defaultPosition, time).WaitForCompletion();
+    }
+
+    public virtual IEnumerator ResetCameraPositionSpeed(int speed = 1)
+    {
+        if(speed < 1) StopCoroutine(ResetCameraPosition(speed)); 
+        yield return transform.DOMove(defaultPosition, Vector3.Distance(transform.position, defaultPosition)/speed).WaitForCompletion();
     }
 }
